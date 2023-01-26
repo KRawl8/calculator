cc3be4d005d6177854436d02a14409ef2e5cba60
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-01-26 16:57:19 +0000

    Add functionality to number buttons so they show in display

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ const divide = (num1, num2) => {
 const operate = (operationFunction, num1, num2) => {
     return operationFunction(num1, num2);
 }
+
+const smallDisplay = document.querySelector('.secondaryDisplay');
+
+const display = document.querySelector('.display');
+let displayValue = 0;
+display.textContent = displayValue;
+
+const numButtons = document.querySelectorAll('.number');
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (displayValue === 0) {
+            displayValue = button.textContent;
+        } else {
+            displayValue += button.textContent;
+        }
+        
+        display.textContent = displayValue;
+        // console.log(display.textContent);
+        // console.log(button.textContent);
+        // console.log(displayValue);
+    })
+})
+
+const equals = document.querySelector('.equals');
+equals.addEventListener('click', () => {
+    smallDisplay.textContent = displayValue;
+})
