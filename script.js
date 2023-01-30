a71ec54abcb35ee0291de0f712bd7281f635cbe8
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-01-30 09:48:37 +0000

    Add functionality so the calculator does basic sums using 2 numbers

--- script.js
+++ script.js
@@ -18,13 +18,16 @@ const operate = (operationFunction, num1, num2) => {
     return operationFunction(num1, num2);
 }
 
+let firstNum = 0;
+let operationArray = [];
+let displayValue = 0;
+
 const smallDisplay = document.querySelector('.secondaryDisplay');
 
 const display = document.querySelector('.display');
-let displayValue = 0;
 display.textContent = displayValue;
 
-const numButtons = document.querySelectorAll('.number');
+const numButtons = document.querySelectorAll('.number'); //When the numbers are clicked they update the display
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
         if (displayValue === 0) {
@@ -37,10 +40,44 @@ numButtons.forEach((button) => {
         // console.log(display.textContent);
         // console.log(button.textContent);
         // console.log(displayValue);
+    }); 
+});
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        firstNum = displayValue;
+        smallDisplay.textContent += displayValue;
+        // console.log(firstNum);
+        switch(button.textContent) {
+            case '÷':
+                operationArray[0] = divide;
+                smallDisplay.textContent += ' ÷ '; 
+                break;
+            case 'x':
+                operationArray[0] = multiply;
+                smallDisplay.textContent += ' x ';
+                break;
+            case '-':
+                operationArray[0] = subtract;
+                smallDisplay.textContent += ' - ';
+                break;
+            case '+':
+                operationArray[0] = add;
+                smallDisplay.textContent += ' + ';
+                break;
+        }
+        displayValue = 0;
+        console.log(operationArray);
     })
 })
 
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () => {
-    smallDisplay.textContent = displayValue;
+    smallDisplay.textContent += displayValue;
+    displayValue = operationArray[0](Number(firstNum), Number(displayValue));
+    display.textContent = displayValue;
 })
+
+// let array = [add];
+// console.log(array[0](7,2))
